5e271e0a988eb1af38f951aa33e2d74df49480fb
sharat940 <sharat.o@synup.com>
2020-06-01 01:22:56 +0530

    logic changed to accept multiple numbers

--- lib/dictor.rb
+++ lib/dictor.rb
@@ -1,4 +1,6 @@
+#!/usr/bin/env ruby
 require 'pry'
+require 'set'
 class Dictor
   def initialize(verbose: false, dict:, phones:)
     @dict = dict
@@ -6,75 +8,72 @@ class Dictor
     @verbose = verbose
     @number_map = {}
     @number_suggestions = {}
-    @wordlist = []
     @encoder = {}
     %w{abc def ghi jkl mno pqrs tuv wxyz}.to_enum.with_index(2).each do |letters, index|
       letters.scan(/./).map{|l| @encoder[l] = index}
     end
+
   end
 
   def process!
     log('Loading the data from dictionary file')
     load_dictionary
-    log('Start conversion to fancy phone numbers')
-    convert_phone_numbers
+    log('reading phone numbers')
+    read_phone_numbers
     log('Printing the results')
     print_number_suggestions
   end
 
   def print_number_suggestions
-
+    @number_suggestions.each do |number, values|
+      puts "The number suggestions for #{number} are:"
+      values.to_a.each_with_index do |value,i|
+        puts "#{i+1}: #{value.chomp.upcase}"
+      end
+    end
   end
 
-  def convert_phone_numbers
+  def read_phone_numbers
     @phones.each do |phone|
-      formatted_phone = phone.to_i.to_s
-      @number_suggestions[formatted_phone] ||= []
-      @number_map.keys.each do |number|
-        next unless formatted_phone.include? number
-        number_parts = formatted_phone.partition(number).compact.reject(&''.method(:==))
-        if number_parts.size == 1
-          @number_suggestions[formatted_phone] = @number_map[formatted_phone]
-          break
-        end
-        remaining_nums = number_parts - [number]
-        new_mapper = {}
-        remaining_nums.each do |num|
-          @number_map.keys.each do |new_number|
-            next unless num.include? new_number
-            result = num.partition(new_number).compact.reject(&''.method(:==))
-            if result.size == 1
-              new_mapper[num] = @number_map[formatted_phone]
-            end
-          end
-        end
-        phone_copy = formatted_phone
-        binding.pry
-        phone_copy[number] = @number_map[number].last
-        new_mapper.keys.each do |n|
-          phone_copy[n] = new_mapper[n].last
-        end
-        @number_suggestions[formatted_phone] = phone_copy
-      end
+      phone.gsub!(/[^\d]/, '')
+      next if phone.empty?
+      @wordlist = Set.new
+      create_wordspace(phone)
+      @number_suggestions[phone] = @wordlist
     end
-    binding.pry
+    @number_suggestions
   end
 
-  def recursive_search(formatted_phone, iteration)
-    @number_map.keys.each do |number|
-      return nil unless formatted_phone.include? number
-      value_map = {}
-      number_parts = formatted_phone.partition(number).compact.reject(&''.method(:==))
-      value_map[number] = @number_map[number]
-      if number_parts.size == 1 && iteration.zero?
-        @wordlist.push(@number_map[number])
-          next
-        end
-        remaining_parts = number_parts - [number]
-      remaining_parts.each do |part|
-        recursive_search(part)
+  def build_fancy_number_list(wordspace, phone,  word = '')
+    pointer = word.length - word.count('-')
+    if pointer >= phone.size
+      word.chop! if word.end_with? '-'
+      word.reverse!.chop!.reverse! if word[0] == '-'
+      @wordlist << word
+      return
+    end
+    wordspace[pointer].each do  |number_map_values|
+      next unless number_map_values
+      number_map_values.each do |value|
+        new_word = value.empty? ? "#{value}" : "#{word}-#{value}"
+        build_fancy_number_list(wordspace, phone, new_word)
+        build_fancy_number_list(wordspace, phone,"#{new_word}-#{phone[pointer + value.length]}")
       end
     end
+    @wordlist
+  end
+
+  def create_wordspace(phone)
+    wordspace = Array.new(phone.size)
+    (0..phone.size).each do |i|
+      length_list = wordspace[i] = Array.new(phone.size - i)
+      num_word = ''
+      (i...phone.size).each do |j|
+        num_word << phone[j]
+        length_list[j - i] = @number_map[num_word]
+      end
+    end
+    build_fancy_number_list(wordspace, phone)
   end
 
   def load_dictionary
@@ -82,7 +81,7 @@ class Dictor
       formatted_word = word.downcase
       formatted_number = formatted_word.scan(/./).map{ |l| @encoder[l]}.join.to_s
       @number_map[formatted_number] ||= []
-      @number_map[formatted_number].push(formatted_word)
+      @number_map[formatted_number].push(formatted_word.chomp)
     end
     @number_map
   end
